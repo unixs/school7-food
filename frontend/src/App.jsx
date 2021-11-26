ff563a7f9a3d98e6ee9c4959e7e77d64cd8b9f82
Alexander Feodorov <webmaster@unixcomp.org>
2021-11-26 20:09:02 +0300

    Done frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,37 +12,33 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import {useEffect, useState} from "react";
+import {Button} from "@mui/material";
 
+const DATA_WEB_PATH = "/food/food.json";
 
-function renderFoodTable(data) {
-  console.log(data);
+function loadData(path) {
+  return fetch(path)
+    .then(response => response.json());
+}
 
-  function createData(name) {
-    return { name };
-  }
-
-  const rows = [
-    createData('Frozen yoghurt'),
-    createData('Ice cream sandwich'),
-  ];
-
+function renderFoodTable(data, config) {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: 650 }}>
         <TableHead>
           <TableRow>
-            <TableCell>Дата</TableCell>
             <TableCell>Меню</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {data.map((row) => (
             <TableRow
               key={row.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.name}</TableCell>
+              <TableCell>
+                <Button href={`${config.path}/${row.filename}`}>{row.filename}</Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -90,88 +86,15 @@ export default function FoodTabs() {
   });
 
   useEffect(() => {
-    setTimeout(() => {
-      setData(
-        {
-          config: {
-            path: "/web/path/to/files/"
-          },
-          data: {
-            ss: [
-              {
-                filename: "2021-11-26-ss.xlsx",
-                ctime: {
-                  date: "2021-11-26 00:00:00.000000",
-                  timezone_type: 1,
-                  timezone: "+00:00"
-                },
-                flag: "ss",
-                md5: "6d7fce9fee471194aa8b5b6e47267f03"
-              },
-              {
-                filename: "2021-11-24-ss.xlsx",
-                ctime: {
-                  date: "2021-11-24 00:00:00.000000",
-                  timezone_type: 1,
-                  timezone: "+00:00"
-                },
-                flag: "ss",
-                md5: "b026324c6904b2a9cb4b88d6d61c81d1"
-              },
-              {
-                filename: "2021-11-23-ss.xlsx",
-                ctime: {
-                  date: "2021-11-23 00:00:00.000000",
-                  timezone_type: 1,
-                  timezone: "+00:00"
-                },
-                flag: "ss",
-                md5: "6d7fce9fee471194aa8b5b6e47267f03"
-              }
-            ],
-            sm: [
-              {
-                filename: "2021-11-26-sm.xlsx",
-                ctime: {
-                  date: "2021-11-26 00:00:00.000000",
-                  timezone_type: 1,
-                  timezone: "+00:00"
-                },
-                flag: "sm",
-                md5: "6d7fce9fee471194aa8b5b6e47267f03"
-              },
-              {
-                filename: "2021-11-24-sm.xlsx",
-                ctime: {
-                  date: "2021-11-24 00:00:00.000000",
-                  timezone_type: 1,
-                  timezone: "+00:00"
-                },
-                flag: "sm",
-                md5: "b026324c6904b2a9cb4b88d6d61c81d1"
-              },
-              {
-                filename: "2021-11-23-sm.xlsx",
-                ctime: {
-                  date: "2021-11-23 00:00:00.000000",
-                  timezone_type: 1,
-                  timezone: "+00:00"
-                },
-                flag: "sm",
-                md5: "6d7fce9fee471194aa8b5b6e47267f03"
-              }
-            ]
-          }
-        }
-      );
-    }, 1000);
+    loadData(DATA_WEB_PATH)
+      .then(data => setData(data));
   }, [])
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const { data: { ss, sm } } = data;
+  const { data: { ss, sm }, config } = data;
 
   return (
     <Box sx={{ width: '100%' }}>
@@ -182,10 +105,10 @@ export default function FoodTabs() {
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        {renderFoodTable(sm)}
+        {renderFoodTable(sm, config)}
       </TabPanel>
       <TabPanel value={value} index={1}>
-        {renderFoodTable(ss)}
+        {renderFoodTable(ss, config)}
       </TabPanel>
     </Box>
   );
